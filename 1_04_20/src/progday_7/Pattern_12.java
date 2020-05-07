26af11ee5fe9fe4c7fe12ee78061c00b7735ef59
Bigyandutt panda <bigysn.panda123@gmail.com>
2020-05-07 11:15:31 +0530

    fitrst commit thrpugh eclipse directly

---
+++ 1_04_20/src/progday_7/Pattern_12.java
@@ -0,0 +1,25 @@
+package progday_7;
+
+public class Pattern_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i=0;i<5;i++)
+        {
+        	for(int j=0;j<5;j++)
+        	{
+        		if(i==j || i+j==4)
+        		{
+        			System.out.print("*");
+        		}
+        		else 
+        		{
+        		System.out.print(" ");	
+        		}
+        	}
+        	System.out.println(" ");
+        }        
+        
+	}
+
+}
---
+++ 1_04_20/src/progday_7/Pattern_16.java
@@ -0,0 +1,19 @@
+package progday_7;
+
+public class Pattern_16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int count=0;
+          for(int i=1;i<=4;i++)
+          {
+        	  for(int j=1;j<=i;j++)
+        	  {
+        		  count=count+1;
+        		  System.out.print(count);
+        	  }
+        	  System.out.println(" ");
+          }
+	}
+
+}
